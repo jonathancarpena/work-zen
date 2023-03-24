499c0a945de36d6bbd6fe3190d55470750cdcee1
Jonathan <jonathancarpena99@gmail.com>
2023-03-23 17:28:41 -0700

    Fixing Mobile Height Issues

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -10,7 +10,7 @@ interface Props {
 function Layout({ children }: Props) {
 	useVH();
 	return (
-		<div className="flex flex-col items-stretch h-screen  bg-main-light-0 dark:bg-main-dark-0 overflow-hidden font-mono">
+		<div className="app flex flex-col items-stretch h-screen  bg-main-light-0 dark:bg-main-dark-0 overflow-hidden font-mono">
 			<Navbar />
 			<div className="flex-1 bg-main-0 dark:bg-main-dark overflow-x-hidden overflow-y-auto px-3 md:px-4 max-h-fit ">
 				{children}
