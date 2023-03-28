8814f9a7683a91a0842089342b4528d78a3d434b
Jonathan <jonathancarpena99@gmail.com>
2023-03-28 13:42:42 -0700

    Added padding for notch

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,6 @@ interface Props {
 	onClick?: () => void;
 	disabled?: boolean;
 	block?: boolean;
-	icon?: boolean;
 	active?: boolean;
 	size?: 'sm' | 'md' | 'lg';
 	sx?: string;
@@ -18,7 +17,6 @@ function Button({
 	size = 'md',
 	disabled = false,
 	block = true,
-	icon = false,
 	sx,
 }: Props) {
 	return (
@@ -33,13 +31,47 @@ function Button({
 								? 'active:scale-90 hover:bg-white dark:hover:bg-main-dark-lighter'
 								: ''
 					  }  `
-			} ${icon ? 'w-11 md:w-14 rounded-xl' : ''} ${
-				block && !icon ? 'w-full rounded-md' : 'w-max rounded-md px-4'
-			} ${size === 'sm' ? 'h-8 md:h-10' : ''} 		${
-				size === 'md' ? 'h-11 md:h-14' : ''
-			} ${size === 'lg' ? 'h-16 md:h-20' : ''}  
-	
+			}  ${block ? 'w-full rounded-md' : 'w-max rounded-md px-4'} ${
+				size === 'sm' ? 'h-8 md:h-10' : ''
+			} 		${size === 'md' ? 'h-11 md:h-14' : ''} ${
+				size === 'lg' ? 'h-16 md:h-20' : ''
+			}  
+			
 			flex justify-center items-center  transition-transform duration-100 border border-pureBlack dark:border-white text-sm md:text-lg ${sx}`}
+		>
+			{children}
+		</button>
+	);
+}
+
+export function IconButton({
+	children,
+	onClick,
+	active,
+	size = 'md',
+	disabled = false,
+	block = false,
+	sx,
+}: Props) {
+	return (
+		<button
+			disabled={disabled}
+			onClick={onClick}
+			className={`${
+				active
+					? 'bg-main-dark-darker text-white dark:bg-main-light-lighter dark:text-black '
+					: `bg-inherit text-black  dark:text-white  ${
+							!disabled
+								? 'active:scale-90 hover:bg-white dark:hover:bg-main-dark-lighter'
+								: ''
+					  }`
+			}  ${block ? 'w-full' : 'w-11 md:w-14 '} ${
+				size === 'sm' ? 'h-8 md:h-10' : ''
+			} 	${size === 'md' ? 'h-11 md:h-14' : ''} ${
+				size === 'lg' ? 'h-16 md:h-20' : ''
+			}  
+		rounded-lg md:rounded-xl
+			flex justify-center items-center  transition-transform duration-100 border border-pureBlack dark:border-white text-xl md:text-2xl ${sx}`}
 		>
 			{children}
 		</button>
--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -3,7 +3,7 @@ import { useTab, useTabUpdate } from '../../lib/context/Tab';
 
 // Components
 import { FiCrosshair, FiHash, FiEdit2, FiClipboard } from 'react-icons/fi';
-import Button from '../Button';
+import { IconButton } from '../Button';
 
 // Types
 import { TabOptions } from '../../lib/interfaces';
@@ -16,26 +16,23 @@ function Navbar() {
 	const tab = useTab();
 	const handleTabUpdate = useTabUpdate();
 	const NavItems: NavItems[] = [
-		{ value: 'focus', icon: <FiCrosshair className="text-xl md:text-2xl" /> },
-		{ value: 'notes', icon: <FiEdit2 className="text-xl md:text-2xl" /> },
-		{ value: 'tasks', icon: <FiClipboard className="text-xl md:text-2xl" /> },
-		{ value: 'calculator', icon: <FiHash className="text-xl md:text-2xl" /> },
+		{ value: 'focus', icon: <FiCrosshair /> },
+		{ value: 'notes', icon: <FiEdit2 /> },
+		{ value: 'tasks', icon: <FiClipboard /> },
+		{ value: 'calculator', icon: <FiHash /> },
 	];
 	return (
 		<>
-			{/* <div className="w-full h-[20px] bg-gradient-to-b from-black to-transparent hidden standalone:block"></div> */}
-			<nav className="h-16 w-full z-30 px-3 md:px-4 md:h-20   bg-inherit  ">
+			<nav className="h-16 w-full z-30 px-3 md:px-4 md:h-20   bg-inherit  standalone:pt-7">
 				<ul className="flex items-center h-full space-x-3 max-w-screen-2xl mx-auto ">
 					{NavItems.map((item) => (
 						<li key={`NavItem-${item.value}`}>
-							<Button
-								icon
-								block={false}
+							<IconButton
 								active={tab === item.value}
 								onClick={() => handleTabUpdate(item.value)}
 							>
 								{item.icon}
-							</Button>
+							</IconButton>
 						</li>
 					))}
 				</ul>
